fc5db9eb190f97cf506eb5d7b1be07987714da63
RitaVeiga <up202103388@up.pt>
2023-05-28 17:14:30 +0100

    Create FindViewByIdReturnsNullOperatorMutator.js

---
+++ MutatorV2/javascript_src/mutation/FindViewByIdReturnsNullOperatorMutator.js
@@ -0,0 +1,81 @@
+laraImport("lara.mutation.Mutator");
+laraImport("kadabra.KadabraNodes");
+laraImport("weaver.WeaverJps");
+laraImport("weaver.Weaver");
+
+class FindViewByIdReturnsNullOperatorMutator extends Mutator {
+    constructor() {
+        super("FindViewByIdReturnsNullOperatorMutator");
+
+        this.mutationPoints = [];
+        this.currentIndex = 0;
+        this.mutationPoint = undefined;
+        this.previousValue = undefined;
+    }
+    addJp(joinpoint) {
+
+        if (joinpoint.instanceOf('call')
+        ) {
+            if (joinpoint.children[0] == 'findViewById - Executable') {
+
+                this.mutationPoints.push(joinpoint);
+            }
+        }
+        if (this.mutationPoints.length > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    hasMutations() {
+        return this.currentIndex < this.mutationPoints.length;
+    }
+
+    getMutationPoint() {
+        if (this.isMutated) {
+            return this.mutationPoint;
+        } else {
+            if (this.currentIndex < this.mutationPoints.length) {
+                return this.mutationPoints[this.currentIndex];
+            } else {
+                return undefined;
+            }
+        }
+    }
+
+    _mutatePrivate() {
+        this.mutationPoint = this.mutationPoints[this.currentIndex];
+
+
+        this.currentIndex++;
+
+        this.previousValue = this.mutationPoint;
+        this.mutationPoint = this.mutationPoint.insertReplace("null");
+
+
+        println("/*--------------------------------------*/");
+        println("Mutating operator n." + this.currentIndex + ": " + this.previousValue
+            + " to " + this.mutationPoint);
+        println("/*--------------------------------------*/");
+
+
+    }
+    _restorePrivate() {
+
+        this.mutationPoint = this.mutationPoint.insertReplace(this.previousValue);
+        this.previousValue = undefined;
+        this.mutationPoint = undefined;
+    }
+
+    toString() {
+        return `Find View By Id Returns Null Operator Mutator from ${this.previousValue} to ${this.mutationPoint}, current mutation points ${this.mutationPoints}, current mutation point ${this.mutationPoint} and previoues value ${this.previousValue}`;
+    }
+
+    toJson() {
+        return {
+            mutationOperatorArgumentsList: []
+            ,
+            operator: this.name,
+        };
+    }
+}
